7a79ee6cdb4463b4b62db335e86137ac478e04bc
Lohit <lohitaksh14059@iiitd.ac.in>
2015-07-22 03:15:19 +0530

    first test with git webhook

--- webhook/views.py
+++ webhook/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.context_processors import csrf
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
-
+@csrf_exempt
 def home(request):
-	return HttpResponse("Github")
+    ctype = request.META['CONTENT_TYPE']
+    event = request.META['X-Github-Event']
+    signature = request.META['X-Hub-Signature']
+    uid = request.META['X-Github-Delivery']
+    if ctype == 'application/json' and signature :
+    	data=json.loads(request.body)
+    	return JsonResponse({'result':'pass','event':event})
+    else:
+        return JsonResponse({'result':'fail'})
