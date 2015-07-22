c2f58b674aa74a7dd7953469d52b5f26b842915c
Lohit <lohitaksh14059@iiitd.ac.in>
2015-07-22 21:01:33 +0530

    Added git signature verification. still to complete

--- webhook/views.py
+++ webhook/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
-import json
+from django.utils.crypto import constant_time_compare
+import json, hmac
+
+
+def verify(signature, payload):
+    digest = hmac.new('KEYHERE') #Just for testing. In production, we'll import from settings or use an ENV
+    digest.update(payload)
+    key = digest.hexdigest()
+    key = 'sha1=' + key
+    return constant_time_compare(key, signature)
 
 @csrf_exempt
 def home(request):
-    ctype = request.META['CONTENT_TYPE']
-    event = request.META['X-Github-Event']
-    signature = request.META['X-Hub-Signature']
-    uid = request.META['X-Github-Delivery']
-    if ctype == 'application/json' and signature :
-    	data=json.loads(request.body)
-    	return JsonResponse({'result':'pass','event':event})
-    else:
-        return JsonResponse({'result':'fail'})
+    try:
+        headers = request.META
+	signature = headers['HTTP_X_HUB_SIGNATURE']
+	delivery = headers['HTTP_X_GITHUB_DELIVERY']
+	ctype = headers['CONTENT_TYPE']
+	if ctype=='application/json':
+	    payload = request.body
+	    if verify(signature, payload):
+    	        return HttpResponse('TRUE')		
+    except:
+        raise Http404
